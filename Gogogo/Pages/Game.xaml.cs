886afd4fcf0ab1d1c5a2e091a461006ee8736785
REX1389 <rrvlelcc@gmail.com>
2023-07-03 19:32:49 +0200

    Pictures

--- Gogogo/Pages/Game.xaml.cs
+++ Gogogo/Pages/Game.xaml.cs
@@ -24,5 +24,10 @@ namespace Gogogo.Pages
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
